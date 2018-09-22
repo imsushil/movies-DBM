04fe3e2ed7938848551ce6f80e404fd4235d6165
Sushil Yadav <sushil7161@gmail.com>
2018-09-22 16:56:19 +0530

    Added appname to urls and changed port number

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var app = express();
-app.use(express.static(__dirname + "/resources")); // myApp will be the same folder name.
+app.use(express.static(__dirname + "/moviesdbm/resources")); // myApp will be the same folder name.
 app.get("/", function (req, res,next) {
     //res.redirect("/"); 
     console.log("get request for index page"+ __dirname);
-    res.sendFile( __dirname + "/index.html");
+    res.sendFile( __dirname + "/moviesdbm/index.html");
 });
-app.listen(8081, "localhost");
-console.log("MyProject Server is Listening on port 8081");
+app.listen(8080, "localhost");
+console.log("MyProject Server is Listening on port 8080");
