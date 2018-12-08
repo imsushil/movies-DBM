76c53bc1c407f38ea20cdce4ab024df16d9a0a40
Sushil Yadav <sushil7161@gmail.com>
2018-12-08 21:07:29 +0530

    referring to our own newly json file
    This will fetch data from movieslisting.json file which is present in the assets folder

--- resources/components/home/homeService.js
+++ resources/components/home/homeService.js
@@ -1,7 +1,7 @@
 angular.module("movies").service("HomeService", ["$http", "$q", function($http, $q) {
 	this.getMoviesList = function() {
 		var deferred = $q.defer();
-		var promise = $http.get("http://starlord.hackerearth.com/movieslisting").then(function(response) {
+		var promise = $http.get("resources/assets/movieslisting.json").then(function(response) {
 			return response.data;
 		}, function(response) {
 			deferred.reject("Location=HomeService, msg=Some error occurred while fetching movie details!");
